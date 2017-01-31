4cc2a628b98404087ea6d6631609242a034e7eee
Kyle <kylekatarnls@gmail.com>
2017-01-31 21:19:08 +0100

    Add Block and Case tokens handlers tests

---
+++ tests/Phug/Parser/TokenHandler/BlockTokenHandlerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Phug\Test\Parser\TokenHandler;
+
+use Phug\Lexer;
+use Phug\Lexer\Token\TagToken;
+use Phug\Parser\State;
+use Phug\Parser\TokenHandler\BlockTokenHandler;
+use Phug\Test\AbstractParserTest;
+
+/**
+ * @coversDefaultClass Phug\Parser\TokenHandler\BlockTokenHandler
+ */
+class BlockTokenHandlerTest extends AbstractParserTest
+{
+    /**
+     * @covers ::<public>
+     */
+    public function testhandleToken()
+    {
+        $this->assertNodes('block bar', [
+            '[DocumentNode]',
+            '  [BlockNode]',
+        ]);
+    }
+
+    /**
+     * @covers                   ::<public>
+     * @expectedException        \RuntimeException
+     * @expectedExceptionMessage You can only pass block tokens to this token handler
+     */
+    public function testHandleTokenTokenException()
+    {
+        $lexer = new Lexer();
+        $state = new State($lexer->lex('div'));
+        $handler = new BlockTokenHandler();
+        $handler->handleToken(new TagToken(), $state);
+    }
+}
