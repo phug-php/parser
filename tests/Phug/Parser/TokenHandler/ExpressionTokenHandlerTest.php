3de6677d5828b31746a47c3ace36a42a3a2a2a86
KyleKatarn <kylekatarnls@gmail.com>
2017-02-02 13:23:45 +0100

    Unit tests for expression token handler

---
+++ tests/Phug/Parser/TokenHandler/ExpressionTokenHandlerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Phug\Test\Parser\TokenHandler;
+
+use Phug\Lexer;
+use Phug\Lexer\Token\TagToken;
+use Phug\Parser\State;
+use Phug\Parser\TokenHandler\ExpressionTokenHandler;
+use Phug\Test\AbstractParserTest;
+
+/**
+ * @coversDefaultClass Phug\Parser\TokenHandler\ExpressionTokenHandler
+ */
+class ExpressionTokenHandlerTest extends AbstractParserTest
+{
+    /**
+     * @covers ::<public>
+     */
+    public function testhandleToken()
+    {
+        $this->assertNodes("p=foo()", [
+            '[DocumentNode]',
+            '  [ElementNode]',
+            '    [ExpressionNode]',
+        ]);
+        $this->assertNodes("p\n  =foo()", [
+            '[DocumentNode]',
+            '  [ElementNode]',
+            '    [ExpressionNode]',
+        ]);
+    }
+
+    /**
+     * @covers                   ::<public>
+     * @expectedException        \RuntimeException
+     * @expectedExceptionMessage You can only pass expression tokens to this token handler
+     */
+    public function testHandleTokenTokenException()
+    {
+        $lexer = new Lexer();
+        $state = new State($lexer->lex(''));
+        $handler = new ExpressionTokenHandler();
+        $handler->handleToken(new TagToken(), $state);
+    }
+}
