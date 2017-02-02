d9bc76de8cb68c9c3d2861378dfd38e0ef19b47c
Kyle <kylekatarnls@gmail.com>
2017-02-02 19:30:35 +0100

    Test getAttribute element node method

---
+++ tests/Phug/Parser/TokenHandler/OutdentTokenHandlerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Phug\Test\Parser\TokenHandler;
+
+use Phug\Lexer;
+use Phug\Lexer\Token\TagToken;
+use Phug\Parser\State;
+use Phug\Parser\TokenHandler\OutdentTokenHandler;
+use Phug\Test\AbstractParserTest;
+
+/**
+ * @coversDefaultClass Phug\Parser\TokenHandler\OutdentTokenHandler
+ */
+class OutdentTokenHandlerTest extends AbstractParserTest
+{
+    /**
+     * @covers ::<public>
+     */
+    public function testhandleSingleLine()
+    {
+        $this->assertNodes("p\n  p\n    div\n  ()\n.foo", [
+            '[DocumentNode]',
+            '  [ElementNode]',
+            '    [ElementNode]',
+            '      [ElementNode]',
+            '    [ElementNode]',
+            '  [ElementNode]',
+        ]);
+    }
+
+    /**
+     * @covers                   ::<public>
+     * @expectedException        \RuntimeException
+     * @expectedExceptionMessage You can only pass outdent tokens to this token handler
+     */
+    public function testHandleTokenTokenException()
+    {
+        $lexer = new Lexer();
+        $state = new State($lexer->lex(''));
+        $handler = new OutdentTokenHandler();
+        $handler->handleToken(new TagToken(), $state);
+    }
+}
