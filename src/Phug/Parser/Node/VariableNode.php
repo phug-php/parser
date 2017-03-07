62c57292d082e0e2833c80f00959abf8cd095c25
Kyle <kylekatarnls@users.noreply.github.com>
2017-03-07 13:59:55 +0100

    Remove Attribute trait from Variable node

--- src/Phug/Parser/Node/VariableNode.php
+++ src/Phug/Parser/Node/VariableNode.php
@@ -3,11 +3,9 @@
 namespace Phug\Parser\Node;
 
 use Phug\Parser\Node;
-use Phug\Util\Partial\AttributeTrait;
 use Phug\Util\Partial\NameTrait;
 
 class VariableNode extends Node
 {
     use NameTrait;
-    use AttributeTrait;
 }
