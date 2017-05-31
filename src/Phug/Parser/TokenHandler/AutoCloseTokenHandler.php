d3d3dd23bde62b9a4681458b38977a803df796f3
KyleKatarn <kylekatarnls@gmail.com>
2017-05-31 21:43:16 +0200

    Cleanup imports

--- src/Phug/Parser/TokenHandler/AutoCloseTokenHandler.php
+++ src/Phug/Parser/TokenHandler/AutoCloseTokenHandler.php
@@ -4,9 +4,7 @@ namespace Phug\Parser\TokenHandler;
 
 use Phug\Lexer\Token\AutoCloseToken;
 use Phug\Lexer\TokenInterface;
-use Phug\Parser\Node\AttributeNode;
 use Phug\Parser\Node\ElementNode;
-use Phug\Parser\Node\MixinCallNode;
 use Phug\Parser\State;
 use Phug\Parser\TokenHandlerInterface;
 
